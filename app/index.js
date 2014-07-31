c14e36ababf6a84819967e3f60b06fa43a663066
Shruti Sharma <shrutijalewar@gmail.com>
2014-07-31 16:34:03 -0500

    final commit

--- app/index.js
+++ app/index.js
@@ -29,7 +29,22 @@ if(calculation === '*'){calc = x*y;}else
 if(calculation === '/'){calc = x/y;}
   
   res.render('calc', {x:x, y:y, calculation:calculation, calc:calc});
+});
 
+app.get('/boxes', function(req,res){
+  res.render('boxes');
+});
+app.post('/boxes', function(req,res){
+  var colors =req.body.colors.split(',');
+  var h = req.body.height.split('-');
+  var w = req.body.width.split('-');
+  var n = req.body.n *1;
+  w = w.map(function(n){return n* 1;});
+  h = h.map(function(n){return n* 1;});
+
+
+  res.render('nboxes', {h:h, w:w, n:n, colors:colors});
+console.log(h);
 });
 
 var port = process.env.PORT;
